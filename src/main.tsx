17f1f51c4613cff4e24009bd1cae4704de927e42
Jhonn Rojas <jhonngranadosrojas@email.com>
2025-11-16 16:41:20 -0600

    Cambios en el toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { route } from './router/route'
 import { AuthProvider } from './modules/auth/providers/AuthProvider'
 import { queryClient } from './lib/queryClient'
+import 'react-hot-toast';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
